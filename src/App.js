6f4bd52b5df818621dd6630834db5d2038fbed53
Ichooo <arnesson.pontus@gmail.com>
2019-08-21 20:07:20 +0200

    Created react app and cleaned up files

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <p className="Start">
+          Hello portfolio
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
