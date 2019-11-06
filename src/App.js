a010dd15416d1bbe2c9ac995bad54dce72f84522
Ichooo <arnesson.pontus@gmail.com>
2019-11-06 15:43:25 +0100

    Added Google Analytics

--- src/App.js
+++ src/App.js
@@ -1,31 +1,34 @@
-import React, { useState } from 'react';
-import './styles//styles.css';
-import Nav from './components/Nav';
-import AboutMe from './components/pages/AboutMe';
-import PageSwitcher from './components/PageSwitcher';
-import Portfolio from './components/pages/Portfolio';
-import Commitments from './components/pages/Commitments';
-import SocialIcons from './components/SocialIcons';
-import { Route, BrowserRouter as Router } from 'react-router-dom';
+import React, { useState } from "react";
+import "./styles//styles.css";
+import Nav from "./components/Nav";
+import AboutMe from "./components/pages/AboutMe";
+import PageSwitcher from "./components/PageSwitcher";
+import Portfolio from "./components/pages/Portfolio";
+import Commitments from "./components/pages/Commitments";
+import SocialIcons from "./components/SocialIcons";
+import { Route, BrowserRouter as Router } from "react-router-dom";
+import ReactGA from "react-ga";
 
 const App = () => {
+  ReactGA.initialize("UA-151646007-1");
+  ReactGA.pageview(window.location.pathname + window.location.search);
 
   const [currentPage, setCurrentPage] = useState(0);
 
-    return (
-      <Router>
-        <div className="App">
-          <Nav />
-          <SocialIcons />
-          <PageSwitcher currPage={currentPage} setCurrPage={setCurrentPage}/>
-          <div className="main">
-            <Route path="/" component={AboutMe}/>
-            <Route path="/" component={Portfolio}/>
-            <Route path="/" component={Commitments}/>
-          </div>
+  return (
+    <Router>
+      <div className="App">
+        <Nav />
+        <SocialIcons />
+        <PageSwitcher currPage={currentPage} setCurrPage={setCurrentPage} />
+        <div className="main">
+          <Route path="/" component={AboutMe} />
+          <Route path="/" component={Portfolio} />
+          <Route path="/" component={Commitments} />
         </div>
-      </Router>
-    );
-}
+      </div>
+    </Router>
+  );
+};
 
 export default App;
